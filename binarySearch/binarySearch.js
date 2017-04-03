aa2756fa3cdf74268e4e8c6ca60458ca3071acae
Josh Jarmain <jjarmain@gmail.com>
2017-04-02 18:13:24 -0700

    bsearchizzle

---
+++ binarySearch/binarySearch.js
@@ -0,0 +1,23 @@
+const items = [1000,99,1,2,3,4,5,6,7,50,99, 34, 22, 21, 1000, 66, 1234, 333, 999, 12, 555];
+const findMe = 66;
+const binarySearch = (arr, item) => {
+    arr.sort((a,b)=> a-b);
+    if(arr[0] === item)
+        return true;
+    if(arr.length === 1)
+        return false;
+    const mid = Math.ceil(arr.length /2);
+    if(arr[mid] === item)
+        return true;
+    if(arr[mid] < item) {
+        let right = arr.splice(mid);
+        return binarySearch(right, item);
+    }
+    else {
+        left = arr.splice(0,mid);
+        return binarySearch(left, item);
+    }
+}
+
+let foundIt = binarySearch(items, findMe);
+console.log(foundIt);
