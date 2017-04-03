d12e10eb422352a57e8f4366d342a73728bea0be
Josh Jarmain <jjarmain@gmail.com>
2017-04-02 20:39:29 -0700

    fixed mix up

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -1,5 +1,5 @@
 const items = [1000,99,1,2,3,4,5,6,7,50,99, 34, 22, 21, 1000, 66, 1234, 333, 999, 12, 555];
-const findMe = 66;
+const findMe = 22;
 const binarySearch = (arr, item) => {
     arr.sort((a,b)=> a-b);
     if(arr[0] === item)
@@ -9,12 +9,12 @@ const binarySearch = (arr, item) => {
     const mid = Math.ceil(arr.length /2);
     if(arr[mid] === item)
         return true;
-    if(arr[mid] < item) {
-        let right = arr.splice(mid);
+    if (arr[mid] < item) {
+        const right = arr.splice(mid);
         return binarySearch(right, item);
     }
     else {
-        left = arr.splice(0,mid);
+        const left = arr.splice(0,mid);
         return binarySearch(left, item);
     }
 }
